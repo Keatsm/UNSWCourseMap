e776ef6cb8a1ec917cd6a2399b55f4405ef2ca74
Keatsm <keatsm@zoho.com>
2023-12-04 22:38:20 +1100

    progress on scraping

--- HandbookScraper/main.py
+++ HandbookScraper/main.py
@@ -3,22 +3,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Set up the browser (in this case, Chrome)
-driver = webdriver.Chrome('path_to_chromedriver')
+driver = webdriver.Chrome()
 
 # Navigate to UNSW course booklet
 driver.get('https://www.handbook.unsw.edu.au/search')
 
-# Find and click the button that loads content
+# Press 'Course'
 button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, 'react-tabs-12')))
+    EC.element_to_be_clickable((By.ID, 'react-tabs-6')))
+button.click()
+
+# Press 'Subject Area'
+button = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//div[@style='overflow: hidden;']/div[6]/div[1]")))
+button.click()
+
+
+search_bar = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//div[@class='css-e2gxlx-SAccordionContentContainer e1450wuy10']/div/div[@class='css-e3eej2-SearchInputContainer eanr10p1']/input[1]"))
+)
+
+# Input text into the search bar
+search_bar.send_keys('COMP')
+
+button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//ul[@class='css-zia84r-StyledOptionsList e15kvt300']/li[2]")))
 button.click()
 
 
 
-# Wait for content to load (if necessary)
-# Scrape data after the content is loaded
-# Example: data = driver.find_element_by_xpath('xpath_to_desired_data').text
-
 # Close the browser
-driver.quit()
+driver.close()
