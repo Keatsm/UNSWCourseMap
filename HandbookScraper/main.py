dc8dc17650a48c4bd56f9ef493c02a9ecfd5cec9
Keatsm <keatsm@zoho.com>
2023-12-11 22:23:15 +1100

    fixed bug and added SENG

--- HandbookScraper/main.py
+++ HandbookScraper/main.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome()
 
 # Navigate to UNSW course booklet
@@ -21,37 +19,52 @@ button = WebDriverWait(driver, 10).until(
 button.click()
 
 # Locate the subject area search bar
-search_bar = WebDriverWait(driver, 10).until(
+searchBar = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, "//div[@class='css-e2gxlx-SAccordionContentContainer e1450wuy10']/div/div[@class='css-e3eej2-SearchInputContainer eanr10p1']/input[1]"))
 )
 
 # Input text into the search bar
-search_bar.send_keys('COMP')
+searchBar.send_keys('COMP')
 
 # Click on COMP
 button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//button[descendant::strong[text()='COMP']]")))
 driver.execute_script("arguments[0].click();", button)
 
+
+
+# Clear search field
+button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[@id='closeMiniSearch']")))
+driver.execute_script("arguments[0].click();", button)
+
+# Input SENG into the search bar
+searchBar.send_keys('SENG')
+
+# Click on SENG
+button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[descendant::strong[text()='SENG']]")))
+driver.execute_script("arguments[0].click();", button)
+
 # Find the parent div for the search results
-parent_div = WebDriverWait(driver, 10).until(
+parentDiv = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, "//div[@id='search-results']")))
 
-# Set what the title is for the course that is initially at the search
-prev_title = parent_div.find_element(By.XPATH, "//div[1]/a/span/span/span").text
+prevTitle = None
+
 
 # Check if search has changed
 def check_for_search_changes(_):
-    global prev_title, parent_div
+    global prevTitle, parentDiv
     try:
-        parent_div = driver.find_element(By.XPATH, "//div[@id='search-results']")
-        title = parent_div.find_element(By.XPATH, "//div[1]/a/span/span/span")
+        parentDiv = driver.find_element(By.XPATH, "//div[@id='search-results']")
+        title = parentDiv.find_element(By.XPATH, "//div[1]/a/span/span/span")
     except Exception:
         print(Exception)
         return False
-    check = prev_title != title.text
-    print(prev_title, title.text)
-    prev_title = title.text
+    check = prevTitle != title.text
+    print(prevTitle, title.text)
+    prevTitle = title.text
     return check
 
 # Wait for search results to update
@@ -62,7 +75,7 @@ morePages = True
 # Repeat while there are more pages to go through
 while morePages:
     # Find all child div elements of the parent div
-    courses = parent_div.find_elements(By.XPATH, "./div")
+    courses = parentDiv.find_elements(By.XPATH, "./div")
 
     # Loop through each child div element
     for course in courses:
@@ -70,23 +83,22 @@ while morePages:
         a_tag = course.find_element(By.XPATH, ".//a")
         
         # Get the href attribute value of the <a> tag
-        href_value = a_tag.get_attribute("href")
+        hrefValue = a_tag.get_attribute("href")
         
         # Print the href value
-        print(href_value)
+        print(hrefValue)
         
     # Check if the right arrow to change page exists (which implies that there are more pages)
-    morePages = len(driver.find_elements(By.XPATH, "//i[text()='keyboard_arrow_right']")) > 0
+    morePages = driver.find_element(By.XPATH, "//button[@aria-label='Go forward 1 page in results']").is_enabled()
     if morePages:
-        right_arrow = driver.find_element(By.XPATH, "//i[text()='keyboard_arrow_right']")
+        rightArrow = driver.find_element(By.XPATH, "//i[text()='keyboard_arrow_right']")
         # Click arrow
-        driver.execute_script("arguments[0].click();", right_arrow)
+        driver.execute_script("arguments[0].click();", rightArrow)
         # Wait for update
         WebDriverWait(driver, 30).until(check_for_search_changes)
 
 # Courses that won't appear on this list that we want to include
-# Include SENG in the above search?
-manual_courses = []
+manualCourses = []
 
 
 # Close the browser
