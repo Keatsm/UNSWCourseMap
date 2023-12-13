38844cb918d3f82d2483b80164867619727c4ab7
Keatsm <keatsm@zoho.com>
2023-12-13 22:35:40 +1100

    began implementing graph construction

---
+++ HandbookScraper/createGraph.py
@@ -0,0 +1,37 @@
+import networkx as nx
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def initNode(graph, url):
+    try:
+        print(url)
+        driver = webdriver.Chrome()
+        driver.get(url)
+        courseCode = driver.find_element(By.XPATH, "//div[@class='css-u09y4s-Box-Flex-StyledFlex e3iudi70']/div[1]/h5").text
+        courseTitle = driver.find_element(By.XPATH, "//h2").text
+        # Not all courses has prerequisites; if the conditions box is not found, assume empty
+        try:
+            prereqs = driver.find_element(By.XPATH, "//div[@id='ConditionsforEnrolment']/div[2]/div").text
+        except:
+            prereqs = ''
+        print(courseCode, courseTitle, prereqs)
+        graph.add_node(courseCode)
+        driver.close()
+    except:
+        # If we get blocked, try again after 5 seconds
+        time.sleep(5)
+        initNode(graph, url)
+
+def createGraph(urls):
+    graph = nx.Graph()
+    for url in urls:
+        initNode(graph, url)
+    
+    
+    
+    
+    
+    return graph
--- HandbookScraper/main.py
+++ HandbookScraper/main.py
@@ -1,4 +1,6 @@
 from getPages import getPages
+from createGraph import createGraph
 
 if __name__ == '__main__':
-    print(getPages())
+    graph = createGraph(getPages())
+    print(graph)
