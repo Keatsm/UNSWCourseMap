7511965124fbaf50cbd5760e48343666af06f5b8
Keatsm <keatsm@zoho.com>
2023-12-17 23:44:19 +1100

    added offering terms and field of education to nodes, fixed some bugs and started creating edges

--- HandbookScraper/createGraph.py
+++ HandbookScraper/createGraph.py
@@ -1,9 +1,9 @@
 import networkx as nx
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+
+orNodes = 0
 
 def initNode(graph, url):
     try:
@@ -12,22 +12,39 @@ def initNode(graph, url):
         driver.get(url)
         courseCode = driver.find_element(By.XPATH, "//div[@class='css-u09y4s-Box-Flex-StyledFlex e3iudi70']/div[1]/h5").text
         courseTitle = driver.find_element(By.XPATH, "//h2").text
+        try:
+            offeringTerms = driver.find_element(By.XPATH, "//div[child::h4[text()='Offering Terms']]/div/div").text
+        except:
+            offeringTerms = ''
+        try:
+            field = driver.find_element(By.XPATH, "//div[child::h4[text()='Field of Education']]/div/div").text
+        except:
+            field = ''
         # Not all courses have prerequisites; if the conditions box is not found, assume empty
         try:
             prereqs = driver.find_element(By.XPATH, "//div[@id='ConditionsforEnrolment']/div[2]/div").text
         except:
             prereqs = ''
-        print(courseCode, courseTitle, prereqs)
-        graph.add_node(courseCode)
+        print(courseCode, courseTitle, prereqs, offeringTerms, field)
+        graph.add_node(courseCode, attr={'courseTitle' : courseTitle, 'prereqs' : prereqs, 'offeringTerms' : offeringTerms, 'field' : field})
         driver.close()
-    except:
+    except Exception as e:
+        print(str(e))
         # If we get blocked, try again after 5 seconds
+        driver.close()
         time.sleep(5)
         initNode(graph, url)
+        
+def createEdges(graph, node, orNodes):
+    prereqString = node['attr']['prereqs']
+    if prereqString.startswith('Exlusion') or prereqString == '':
+        return
 
 def createGraph(urls):
     graph = nx.Graph()
     for url in urls:
         initNode(graph, url)
+    # orNodes to be added to graph after we have looped through existing nodes
+    orNodes = []
     
     return graph
