297c13894104efee79bc710576e5376bcf4301f7
Keatsm <keatsm@zoho.com>
2024-02-15 23:33:25 +1100

    added backup cache

--- Backend/Server/app.py
+++ Backend/Server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from neo4j import GraphDatabase
 from dotenv import load_dotenv, find_dotenv
-import os
+import os, json
 from flask import Flask
 from flask_cors import CORS
 
@@ -9,6 +9,9 @@ app = Flask(__name__)
 CORS(app, origins="https://unsw-course-map.vercel.app")
 
 load_dotenv(find_dotenv())
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(current_directory)
 
 # Connect to Neo4j
 driver = GraphDatabase.driver(os.getenv("NEO4J_URI"), auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")))
@@ -19,36 +22,44 @@ def getGraph():
     node_query = "MATCH (n) RETURN n"
     nodes = []
 
-    with driver.session() as session:
-        node_result = session.run(node_query)
+    try:
+        with driver.session() as session:
+            node_result = session.run(node_query)
 
-        for record in node_result:
-            node = record['n']
-            nodes.append({
-                "id": node.id,  # Use node.id directly for the node ID
-                "labels": list(node.labels),  # Get the node labels
-                "properties": dict(node)  # Get all node properties
-            })
+            for record in node_result:
+                node = record['n']
+                nodes.append({
+                    "id": node.id,  # Use node.id directly for the node ID
+                    "labels": list(node.labels),  # Get the node labels
+                    "properties": dict(node)  # Get all node properties
+                })
 
-    # Retrieve relationships from Neo4j
-    relationship_query = "MATCH ()-[r]->() RETURN r"
-    relationships = []
+        # Retrieve relationships from Neo4j
+        relationship_query = "MATCH ()-[r]->() RETURN r"
+        relationships = []
 
-    with driver.session() as session:
-        relationship_result = session.run(relationship_query)
+        with driver.session() as session:
+            relationship_result = session.run(relationship_query)
 
-        for record in relationship_result:
-            relationship = record['r']
-            relationships.append({
-                "startNode": relationship.start_node.id,
-                "endNode": relationship.end_node.id,
-                "type": relationship.type
-            })
+            for record in relationship_result:
+                relationship = record['r']
+                relationships.append({
+                    "startNode": relationship.start_node.id,
+                    "endNode": relationship.end_node.id,
+                    "type": relationship.type
+                })
 
-    return jsonify({
-        "nodes": nodes,
-        "relationships": relationships
-    })
+        result = {
+            "nodes": nodes,
+            "relationships": relationships
+        }
+        with open('cache.json', 'w') as file:
+            json.dump(result, file, indent=4)
+    except:
+        with open('cache.json', 'r') as file:
+            result = json.load(file)
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
